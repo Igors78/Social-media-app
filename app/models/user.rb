9ea4516b7957999f993a6d6a15b8cf80bae4a483
Igors78 <oleinikovsigors@gmail.com>
2021-01-14 13:12:37 +0200

    Add model associations for user and friendship tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,28 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :friend_requests_sent, -> { where(status: false) },
+           class_name: 'Friendship'
+  has_many :requests_sent_to, through: :friend_requests_sent, source: :friend
+
+  has_many :friend_requests_received, -> { where(status: false) },
+           class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :requests_received_from, through: :friend_requests_received,
+                                    source: :user
+
+  has_many :accepted_friendships, -> { where(status: true) },
+           class_name: 'Friendship'
+  # Associations
+  # friendship.user => sender of request
+  # friendship.friend => receiver of request
+  # user.friends => friends invited by user(receivers of request)
+  # user.friend_requests_sent => unconfirmed friendships (from sender side)
+  # user.requests_sent_to => receivers(friends) of request
+  # user.friend_requests_received => unconfirmed friendships (from receiver side)
+  # user.requests_received_from => senders of request
+  # user.accepted_friendships => accepted friendships(status:true)
 end
