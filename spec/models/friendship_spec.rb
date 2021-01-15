b64cd05d5ca0e177a4a69acf602bea83eac64421
CornerstoneII <seunmyles@gmail.com>
2021-01-15 15:49:19 +0100

    Add integration test for milestone5

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(
+      friend_id: ''
+    )
+  end
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:friend).class_name('User') }
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,12 +24,9 @@ require 'capybara/rspec'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Schema.verbose = false
+load "#{Rails.root}/db/schema.rb"
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
