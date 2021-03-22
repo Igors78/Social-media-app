ed1cdc47ba2cca67413acdaa59f9ca3e50ca898d
CornerstoneII <seunmyles@gmail.com>
2021-03-22 15:09:26 +0100

    Add inverse_friendship association to the friendship_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,42 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 20 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
 
-  has_many :friend_requests_sent, -> { where(status: false) },
-           class_name: 'Friendship'
-  has_many :requests_sent_to, through: :friend_requests_sent,
-                              source: :friend
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  has_many :friend_requests_received, -> { where(status: false) },
-           class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :requests_received_from, through: :friend_requests_received,
-                                    source: :user
-
-  has_many :accepted_friendships, -> { where(status: true) },
-           class_name: 'Friendship'
-  has_many :active_friends, through: :accepted_friendships,
-                            source: :user
-
-  has_many :active_friends, through: :accepted_friendships,
-                            source: :friend
-
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :inverse_friends, through: :inverse_friendships, source: :user
-  # Associations
-  # friendship.user => sender of request
-  # friendship.friend => receiver of request
-  # user.friends => friends invited by user(receivers of request)
-  # user.friend_requests_sent => unconfirmed friendships (from sender side)
-  # user.requests_sent_to => receivers(friends) of request
-  # user.friend_requests_received => unconfirmed friendships (from receiver side)
-  # user.requests_received_from => senders of request
-  # user.accepted_friendships => accepted friendships(status:true)
+  has_many :inverse_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverse_friendships
 end
